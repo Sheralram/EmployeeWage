215fa7e4f02b9c6a56f50345d18b6558f2609ccd
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-08 21:00:39 +0530

    Calculating for a month as 20days

--- src/com/ram/EmployeeWageprogram.java
+++ src/com/ram/EmployeeWageprogram.java
@@ -11,36 +11,37 @@ public class EmployeeWageprogram {
         int Wageperhour = 20;
         int Fulldayhour = 8;
         int Parttimehour = 4;
+        int Month=20;
         Random random = new Random();
         int Empcheck = random.nextInt(3);
-        System.out.println("A random int from 0 to 1: " +  Empcheck);
+        System.out.println("A random int from 0 to 2: " +  Empcheck);
         if (Empcheck == is_Fullpresent) {
             System.out.println("Employee is Present");
-            int DailyEmpWage=Wageperhour*Fulldayhour;
-                    System.out.println("Employee Daily Wage is " +  DailyEmpWage);
+            int DailyEmpWage=Wageperhour*Fulldayhour*Month;
+                    System.out.println("Employee Monthly Wage is " +  DailyEmpWage);
         } else if(Empcheck == is_Parttime) {
             System.out.println("Employee is  Part time Present");
-            int ParttimeEmpWage=Wageperhour*Parttimehour;
+            int ParttimeEmpWage=Wageperhour*Parttimehour*Month;
             System.out.println("Employee Parttime Wage is " + ParttimeEmpWage);
         }
         else{
             System.out.println("Employee is absent");
-            System.out.println("Employee Daily Wage is Zero");
+            System.out.println("Employee Monthly Wage is Zero");
         }
         Scanner read = new Scanner(System.in);
         System.out.print("Enter any number between 0 and 2: ");
         int num = read.nextInt();
            switch (num){
        case 1: System.out.println("Employee is Present");
-           int DailyEmpWage=Wageperhour*Fulldayhour;
-           System.out.println("Employee Daily Wage is " +  DailyEmpWage);
+           int DailyEmpWage=Wageperhour*Fulldayhour*Month;
+           System.out.println("Employee Monthly Wage is " +  DailyEmpWage);
            break;
        case 2: System.out.println("Employee is  Part time Present");
-           int ParttimeEmpWage=Wageperhour*Parttimehour;
+           int ParttimeEmpWage=Wageperhour*Parttimehour*Month;
            System.out.println("Employee Parttime Wage is " + ParttimeEmpWage);
            break;
        default : System.out.println("Employee is absent");
-           System.out.println("Employee Daily Wage is Zero");
+           System.out.println("Employee Monthly Wage is Zero");
 
    }
     }
