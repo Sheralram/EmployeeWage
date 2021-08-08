76d99229a815ae0392eef74545d73c9b136cd9f5
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-08 20:24:10 +0530

    Part time Employee & Wage

--- src/com/ram/EmployeeWageprogram.java
+++ src/com/ram/EmployeeWageprogram.java
@@ -5,16 +5,24 @@ import java.util.Random;
 public class EmployeeWageprogram {
     public static void main(String[] args) {
         System.out.println("Welcome to EmployeeWage program");
+        int is_Fullpresent=1;
+        int is_Parttime=2;
         int Wageperhour = 20;
         int Fulldayhour = 8;
+        int Parttimehour = 4;
         Random random = new Random();
-        int Empcheck = random.nextInt(2);
+        int Empcheck = random.nextInt(3);
         System.out.println("A random int from 0 to 1: " +  Empcheck);
-        if (Empcheck == 1) {
+        if (Empcheck == is_Fullpresent) {
             System.out.println("Employee is Present");
             int DailyEmpWage=Wageperhour*Fulldayhour;
                     System.out.println("Employee Daily Wage is " +  DailyEmpWage);
-        } else {
+        } else if(Empcheck == is_Parttime) {
+            System.out.println("Employee is  Part time Present");
+            int ParttimeEmpWage=Wageperhour*Parttimehour;
+            System.out.println("Employee Parttime Wage is " + ParttimeEmpWage);
+        }
+        else{
             System.out.println("Employee is absent");
             System.out.println("Employee Daily Wage is Zero");
         }
