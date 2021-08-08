30620ccf8ec7e657a5a5f62aca722967aa5413c7
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-08 19:46:12 +0530

    Employee Attendance

--- src/com/ram/EmployeeWageprogram.java
+++ src/com/ram/EmployeeWageprogram.java
@@ -1,8 +1,20 @@
 package com.ram;
 
+import java.util.Random;
+
 public class EmployeeWageprogram {
-    public static  void main (String [] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Welcome to EmployeeWage program");
+        int Wageperhour = 20;
+        int Fulldayhour = 8;
+        Random random = new Random();
+        int Empcheck = random.nextInt(2);
+        System.out.println("A random int from 0 to 1: " +  Empcheck);
+        if (Empcheck == 1) {
+            System.out.println("Employee is Present");
+
+        } else {
+            System.out.println("Employee is absent");
+        }
     }
 }
