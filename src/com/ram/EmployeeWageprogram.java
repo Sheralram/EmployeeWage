5efaf206f3d239d3cda22ce7dcb42cc189cd284b
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-08 20:12:46 +0530

    Employee Daily Wages

--- src/com/ram/EmployeeWageprogram.java
+++ src/com/ram/EmployeeWageprogram.java
@@ -12,9 +12,11 @@ public class EmployeeWageprogram {
         System.out.println("A random int from 0 to 1: " +  Empcheck);
         if (Empcheck == 1) {
             System.out.println("Employee is Present");
-
+            int DailyEmpWage=Wageperhour*Fulldayhour;
+                    System.out.println("Employee Daily Wage is " +  DailyEmpWage);
         } else {
             System.out.println("Employee is absent");
+            System.out.println("Employee Daily Wage is Zero");
         }
     }
 }
